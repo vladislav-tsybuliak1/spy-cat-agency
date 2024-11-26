29cbc675acda92e43924dc2dc528c3ed9640a99f
Vladyslav Tsybuliak <vladislav.tsybuliak@gmail.com>
2024-11-26 11:47:23 +0200

    Add SpyCat model

--- core/models.py
+++ core/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class SpyCat(models.Model):
+    name = models.CharField(max_length=255)
+    years_of_experience = models.PositiveIntegerField()
+    breed = models.CharField(max_length=63)
+    salary = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self) -> str:
+        return str(self.name)
+
