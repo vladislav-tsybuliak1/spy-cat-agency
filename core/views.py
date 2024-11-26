3f400d18171ee238496b8e1249e447f6d460b34f
Vladyslav Tsybuliak <vladislav.tsybuliak@gmail.com>
2024-11-26 12:54:30 +0200

    Add SpyCatViewSet

--- core/views.py
+++ core/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from core.models import SpyCat
+from core.serializers import SpyCateSerializer
+
+
+class SpyCatViewSet(viewsets.ModelViewSet):
+    queryset = SpyCat.objects.all()
+    serializer_class = SpyCateSerializer
