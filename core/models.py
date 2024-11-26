92549d68a214f4d44236a0952775eec5fd47a0c6
Vladyslav Tsybuliak <vladislav.tsybuliak@gmail.com>
2024-11-26 12:41:01 +0200

    Add breed validation to SpyCat model

--- core/models.py
+++ core/models.py
@@ -1,5 +1,8 @@
+from django.core.exceptions import ValidationError
 from django.core.validators import MinValueValidator
 from django.db import models
+
+from core.utils import get_valid_breeds
 
 
 class SpyCat(models.Model):
@@ -16,6 +19,22 @@ class SpyCat(models.Model):
 
     def __str__(self) -> str:
         return str(self.name)
+
+    @staticmethod
+    def validate_breed_name(breed: str, error_to_raise: type[Exception]):
+        valid_breeds = get_valid_breeds()
+        if breed not in valid_breeds:
+            raise error_to_raise(f"{breed} is not a valid cat breed.")
+
+    def clean(self) -> None:
+        SpyCat.validate_breed_name(
+            breed=str(self.breed),
+            error_to_raise=ValidationError,
+        )
+
+    def save(self, *args, **kwargs) -> None:
+        self.full_clean()
+        super().save(*args, **kwargs)
 
 
 class Mission(models.Model):
