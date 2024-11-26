89491b93b327fc8158873da12e5fb9a199a4d334
Vladyslav Tsybuliak <vladislav.tsybuliak@gmail.com>
2024-11-26 13:51:35 +0200

    Add salary update action for SpyCat

--- core/views.py
+++ core/views.py
@@ -1,14 +1,46 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, mixins, status
+from rest_framework.decorators import action
+from rest_framework.request import Request
+from rest_framework.response import Response
 
 from core.models import SpyCat
-from core.serializers import SpyCatSerializer, SpyCatCreateSerializer
+from core.serializers import (
+    SpyCatSerializer,
+    SpyCatCreateSerializer,
+    SpyCatUpdateSalarySerializer,
+)
 
 
-class SpyCatViewSet(viewsets.ModelViewSet):
+class SpyCatViewSet(
+    mixins.ListModelMixin,
+    mixins.CreateModelMixin,
+    mixins.RetrieveModelMixin,
+    mixins.DestroyModelMixin,
+    viewsets.GenericViewSet,
+):
     queryset = SpyCat.objects.all()
     serializer_class = SpyCatSerializer
 
     def get_serializer_class(self) -> type[SpyCatSerializer]:
-        if self.action in ["create"]:
+        if self.action == "create":
             return SpyCatCreateSerializer
         return self.serializer_class
+
+    @action(
+        methods=["PATCH"],
+        detail=True,
+        url_path="update-salary",
+    )
+    def update_salary(self, request: Request, pk: int | None = None) -> Response:
+        spy_cat = self.get_object()
+        serializer = SpyCatUpdateSalarySerializer(
+            spy_cat,
+            data=request.data,
+            partial=True,
+        )
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(
+            SpyCatSerializer(spy_cat).data,
+            status=status.HTTP_200_OK,
+        )
