de1008d34b7074e6146b1e3e2cc74bc93a3a3697
Vladyslav Tsybuliak <vladislav.tsybuliak@gmail.com>
2024-11-26 11:58:59 +0200

    Add django-debug-toolbar settings and urls

--- spycat_agency/settings.py
+++ spycat_agency/settings.py
@@ -27,6 +27,9 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+INTERNAL_IPS = [
+    "127.0.0.1",
+]
 
 # Application definition
 
@@ -37,16 +40,16 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-
     # 3rd parties
     "rest_framework",
-
+    "debug_toolbar",
     # custom apps
     "core",
 ]
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    "debug_toolbar.middleware.DebugToolbarMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
