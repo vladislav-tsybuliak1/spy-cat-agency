3d42fd8cbcd28014c49976edb93e4ea5e0926c61
Vladyslav Tsybuliak <vladislav.tsybuliak@gmail.com>
2024-11-26 13:11:31 +0200

    Add create SpyCat endpoint

--- core/serializers.py
+++ core/serializers.py
@@ -1,11 +1,22 @@
 from rest_framework import serializers
+from rest_framework.exceptions import ValidationError
 
 from core.models import (
     SpyCat,
 )
 
 
-class SpyCateSerializer(serializers.ModelSerializer):
+class SpyCatSerializer(serializers.ModelSerializer):
     class Meta:
         model = SpyCat
         fields = ("id", "name", "years_of_experience", "breed", "salary")
+
+
+class SpyCatCreateSerializer(SpyCatSerializer):
+    def validate(self, attrs: dict) -> dict:
+        data = super().validate(attrs=attrs)
+        SpyCat.validate_breed_name(
+            breed=attrs.get("breed"),
+            error_to_raise=ValidationError,
+        )
+        return data
--- core/views.py
+++ core/views.py
@@ -1,9 +1,14 @@
 from rest_framework import viewsets
 
 from core.models import SpyCat
-from core.serializers import SpyCateSerializer
+from core.serializers import SpyCatSerializer, SpyCatCreateSerializer
 
 
 class SpyCatViewSet(viewsets.ModelViewSet):
     queryset = SpyCat.objects.all()
-    serializer_class = SpyCateSerializer
+    serializer_class = SpyCatSerializer
+
+    def get_serializer_class(self) -> type[SpyCatSerializer]:
+        if self.action in ["create"]:
+            return SpyCatCreateSerializer
+        return self.serializer_class
