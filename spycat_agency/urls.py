b440a77616603dc48f56c928a8d8fe7c9e856bd2
Vladyslav Tsybuliak <vladislav.tsybuliak@gmail.com>
2024-11-26 12:46:42 +0200

    Add endpoint for core application

--- spycat_agency/urls.py
+++ spycat_agency/urls.py
@@ -17,9 +17,9 @@ Including another URLconf
 
 from debug_toolbar.toolbar import debug_toolbar_urls
 from django.contrib import admin
-from django.urls import path
-
+from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("api/v1/spycat-agency/", include("core.urls", namespace="spy-cat-agency-api")),
 ] + debug_toolbar_urls()
