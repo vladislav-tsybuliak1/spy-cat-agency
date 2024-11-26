18202405b86e7c5dac20ecb10dc3838ef4e80de6
Vladyslav Tsybuliak <vladislav.tsybuliak@gmail.com>
2024-11-26 17:42:55 +0200

    Add custom pagination

--- spycat_agency/settings.py
+++ spycat_agency/settings.py
@@ -130,3 +130,5 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+REST_FRAMEWORK = {"DEFAULT_PAGINATION_CLASS": "core.pagination.SpyCatAgencyPaginator"}
