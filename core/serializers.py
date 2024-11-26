d82506a5b525513d728b5a331a5efe653606346e
Vladyslav Tsybuliak <vladislav.tsybuliak@gmail.com>
2024-11-26 14:36:00 +0200

    Add MissionSerializer

--- core/serializers.py
+++ core/serializers.py
@@ -4,6 +4,7 @@ from rest_framework.exceptions import ValidationError
 from core.models import (
     SpyCat,
     Target,
+    Mission,
 )
 
 
@@ -33,3 +34,15 @@ class TargetSerializer(serializers.ModelSerializer):
     class Meta:
         model = Target
         fields = ("id", "name", "country", "notes", "is_complete")
+
+
+class MissionSerializer(serializers.ModelSerializer):
+    targets = TargetSerializer(
+        many=True,
+        read_only=False,
+        allow_empty=False,
+    )
+
+    class Meta:
+        model = Mission
+        fields = ("id", "assigned_cat", "is_complete", "targets")
