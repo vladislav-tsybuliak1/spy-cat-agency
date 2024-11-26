f12f657f2c9ed0c640cd09dee9932b4abc22c0ba
Vladyslav Tsybuliak <vladislav.tsybuliak@gmail.com>
2024-11-26 12:57:42 +0200

    Add SpyCatViewSet to urls

--- core/urls.py
+++ core/urls.py
@@ -1,6 +1,12 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+
+from core.views import SpyCatViewSet
 
 
-urlpatterns = []
+router = DefaultRouter()
+router.register("cats", SpyCatViewSet)
+
+urlpatterns = [path("", include(router.urls))]
 
 app_name = "core"
